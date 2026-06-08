fe92bce009419cbd1161d66e8a121a59385de695
krystalcode26 <288126864+krystalcode26@users.noreply.github.com>
2026-06-08 12:38:58 -0400

    upload handwriting

---
+++ Course/June4/src/main/java/com/yuhsuan/june4/June4Application.java
@@ -0,0 +1,16 @@
+package com.yuhsuan.june4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class June4Application {
+
+  public static void main(String[] args) {
+
+    SpringApplication.run(June4Application.class, args);
+
+    System.out.println("Hello World");
+  }
+
+}
